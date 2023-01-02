4d41c400d4392ae7d092124b256ba1c3e9b6d9c4
DoohyunHwang97 <ghkdengus97@gmail.com>
2023-01-02 19:48:17 +0900

    frontend initialize

---
+++ frontend/.eslintrc.js
@@ -0,0 +1,18 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+    jest: true,
+  },
+  extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended"],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    project: "tsconfig.json",
+    tsconfigRootDir: __dirname,
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  plugins: ["@typescript-eslint"],
+  root: true,
+};
