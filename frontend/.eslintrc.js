0c1ac79f17f016a73bd4c5219e2a887265fc2450
DoohyunHwang97 <ghkdengus97@gmail.com>
2023-01-04 10:48:53 +0900

    eslintprettier settings initialize

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -5,10 +5,15 @@ module.exports = {
     node: true,
     jest: true,
   },
-  extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended"],
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:prettier/recommended",
+    "prettier/@typescript-eslint",
+  ],
   parser: "@typescript-eslint/parser",
   parserOptions: {
-    project: "tsconfig.json",
+    project: "tsconfig.eslint.json",
     tsconfigRootDir: __dirname,
     ecmaVersion: "latest",
     sourceType: "module",
