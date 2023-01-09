5a287e80b84a1ce6af4291ec8b2d363325bf560e
DoohyunHwang97 <ghkdengus97@gmail.com>
2023-01-09 21:48:15 +0900

    backend directory plot initialize

--- backend/matcher/matcher/urls.py
+++ backend/matcher/matcher/urls.py
@@ -18,6 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/user/', include('user.urls')),
-    path('api/item/', include('item.urls')),
+    path('api/common/', include('common.urls')),
 ]
